83436f4b2e8b8c8c06fb3c291171b117a24ffb58
guzuro <guzurogmn@gmail.com>
2021-01-21 22:57:36 +0300

    home section, services section, ideas section

--- js/script.js
+++ js/script.js
@@ -2,5 +2,11 @@
 $(".header-menu__burger").click(function (e) { 
    $(".header-menu__burger").toggleClass("active");
    $(".header-menu").toggleClass("active");
-
 });
+
+$(document).ready(function(){
+   $('.home__slider').slick({
+      dots: true,
+   });
+ });
+ 
